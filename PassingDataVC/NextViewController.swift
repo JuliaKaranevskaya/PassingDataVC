4388c39487c7a9e9a5cb2d90d4482cba1c1dec07
Юлия <yuliya.karanevskaya@gmail.com>
2021-09-14 08:26:11 +0300

    UI setup with constraints

---
+++ PassingDataVC/NextViewController.swift
@@ -0,0 +1,64 @@
+//
+//  NextViewController.swift
+//  PassingDataVC
+//
+//  Created by Юлия Караневская on 13.09.21.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    let textField: UITextField = {
+        let tf = UITextField()
+        tf.backgroundColor = .white
+        tf.text = "Hello"
+        tf.textAlignment = .center
+        return tf
+    }()
+    
+    let button: UIButton = {
+        let b = UIButton()
+        b.backgroundColor = .systemTeal
+        b.setTitle("Save text", for: .normal)
+        b.addTarget(self, action: #selector(saveText), for: .touchUpInside)
+        return b
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+    
+    @objc func saveText() {
+      
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        addUI()
+    }
+    
+    private func addUI() {
+        addTextField()
+        addButton()
+    }
+    
+    private func addTextField() {
+        view.addSubview(textField)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        textField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        textField.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    private func addButton() {
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.topAnchor.constraint(equalTo: textField.bottomAnchor, constant: 50).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+}
--- PassingDataVC/ViewController.swift
+++ PassingDataVC/ViewController.swift
@@ -8,10 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let label: UILabel = {
+        let l = UILabel()
+        l.backgroundColor = .white
+        l.text = "Hello"
+        l.textAlignment = .center
+        return l
+    }()
+    
+    let button: UIButton = {
+        let b = UIButton()
+        b.backgroundColor = .systemBlue
+        b.setTitle("Go to the next page", for: .normal)
+        b.addTarget(self, action: #selector(nextPage), for: .touchUpInside)
+        return b
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+    
+    @objc func nextPage() {
+       
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        addUI()
+    }
+    
+    private func addUI() {
+        addLabel()
+        addButton()
+    }
+    
+    private func addLabel() {
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        label.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    private func addButton() {
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 50).isActive = true
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 200).isActive = true
     }
 
 
